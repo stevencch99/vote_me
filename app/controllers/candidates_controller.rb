c26afe01cfc8ccbdcba8f05b4f4ca503b8dcf900
Nine Bro <shinjisrg@gmail.com>
2019-05-20 16:19:01 +0800

    Use form helper to replace form tag in candidates\/new.html.erb.

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,9 +3,18 @@ class CandidatesController < ApplicationController
   end
 
   def new
+    @candidate = Candidate.new
   end
 
   def create
-    render html: 'abc'
+    # binding.pry
+    # render html: 'abc'
+    candidate_params = params.require(:candidate).permit(:name, :age, :party, :polititcs)
+    @candidate = Candidate.new(candidate_params)
+    
+    if @candidate.save
+      redirect_to '/'
+    else
+    end
   end
 end

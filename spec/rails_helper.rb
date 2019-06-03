a5ed65c1c911e93f2730db1ce0d1da215fc52ecf
Nine Bro <shinjisrg@gmail.com>
2019-06-03 22:28:59 +0800

    Add Product model.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,11 +33,14 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  
+  # 加入 FactoryBot shorthands(for easy methods usage in _spec.rb)
+  config.include.FactoryBot::Syntax::Methods
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
